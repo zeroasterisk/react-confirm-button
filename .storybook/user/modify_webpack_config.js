bff4bba8964943f24ec021de9bfe98d288e12b9c
alan bount <alan@zeroasterisk.com>
2016-06-16 02:30:46 -0400

    bootstrap in storybook

--- .storybook/user/modify_webpack_config.js
+++ .storybook/user/modify_webpack_config.js
@@ -1,4 +1,25 @@
+const path = require('path');
+
 module.exports = function (config) {
   // This is the default webpack config defined in the `../webpack.config.js`
   // modify as you need.
+  config.entry = [
+    'bootstrap-loader',
+    path.resolve(__dirname, '../../'),
+  ];
+  config.resolveLoader = {
+    root: path.resolve(__dirname, '../../'),
+    modulesDirectories: [
+      path.resolve(__dirname, '../../'),
+      path.resolve(__dirname, '../../node_modules/'),
+      path.resolve(__dirname, '../../node_modules/@kadira/storybook/')
+    ],
+  };
+  config.module.loaders.push(
+    {
+      test: /\.scss$/,
+      loaders: ['style', 'css', 'sass'],
+      include: path.resolve(__dirname, '../../'),
+    }
+  );
 };
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,6 +5,7 @@
 // Use `.storybook/user/modify_webpack_config.js instead`.
 
 const path = require('path');
+/*
 const updateConfig = require('./user/modify_webpack_config');
 
 const config = {
@@ -20,4 +21,33 @@ const config = {
 };
 
 updateConfig(config);
+console.log(config);
 module.exports = config;
+*/
+
+// .storybook/webpack.config.js
+
+// Export a function. Accept the base config as the only param.
+module.exports = (storybookBaseConfig, configType) => {
+  // configType has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  // You can change the configuration based on that.
+  // 'PRODUCTION' is used when building the static version of storybook.
+
+  // Make whatever fine-grained changes you need
+  // storybookBaseConfig.module = { ... }
+  storybookBaseConfig.entry.preview.push('bootstrap-loader');
+  storybookBaseConfig.module.loaders.push(
+      {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'}
+  );
+  storybookBaseConfig.module.loaders.push(
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'}
+  );
+  storybookBaseConfig.module.loaders.push(
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'}
+  );
+  storybookBaseConfig.module.loaders.push(
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+  );
+  // Return the altered config
+  return storybookBaseConfig;
+};
