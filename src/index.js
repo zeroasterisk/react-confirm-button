fb5bb21e4a5066919992769a1f8894aeb733ce38
alan bount <alan@zeroasterisk.com>
2016-06-16 02:39:47 -0400

    now supports: active/confirming/disabled

--- src/index.js
+++ src/index.js
@@ -1,69 +1,140 @@
 import React from 'react';
 import classnames from 'classnames';
 
-const buttonStyles = {
-  border: '1px solid #eee',
-  borderRadius: 3,
-  backgroundColor: '#FFFFFF',
-  cursor: 'pointer',
-  fontSize: 15,
-  padding: '3px 10px',
-};
-
 export default class ConfirmButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      clicked: false,
+      is: 'active',
     };
-    this.onClick = props.onClick.bind(this);
-    this.onConfirmClicked = this.handleConfirmClicked.bind(this);
+    this.onClickToggleState = this.handleClick.bind(this);
+    this.onClick = (props.onClick ? props.onClick.bind(this) : () => {});
+    if (props.onConfirm) {
+      this.onConfirm = props.onConfirm.bind(this);
+    } else if (props.confirming && props.confirming.onClick) {
+      this.onConfirm = props.confirming.onClick.bind(this);
+    } else {
+      this.onConfirm = () => {};
+    }
+    // while confirming, when confirmed & disabling
+    this.onDisable = (props.onDisable ? props.onDisable.bind(this) : () => {});
   }
-  handleConfirmClicked() {
-    if (this.state.clicked) {
-      this.setState({ clicked: false });
-      this.onClick();
+  handleClick() {
+    const { disableAfterConfirmed } = this.props;
+    if (this.isConfirming() && disableAfterConfirmed) {
+      // have confirmed and are now disabled
+      this.onConfirm();
+      this.onDisable();
+      this.setState({ is: 'disabled' });
       return;
     }
-    this.setState({ clicked: true });
+    if (this.isConfirming()) {
+      // we are clicking into the active state (from confirming)
+      //   loop around to the beginning...
+      this.onConfirm();
+      this.setState({ is: 'active' });
+      return;
+    }
+    // we are clicking into the confirming state (from active)
+    this.onClick();
+    this.setState({ is: 'confirming' });
+  }
+  isDisabled() {
+    return this.state.is === 'disabled';
+  }
+  isConfirming() {
+    return this.state.is === 'confirming';
+  }
+  isActive() {
+    return (!this.isConfirming() && !this.isDisabled());
   }
   render() {
     const {
-      children,
-      onClick,
-      style,
-      styleConfirm,
-      className,
-      classNameConfirm,
-      text,
-      textConfirm,
+      disableAfterConfirmed,
+      children, // always shows
+      buttonProps,
+      // confirming config
+      confirming,
+      // disabled config
+      disabled,
     } = this.props;
-    const { clicked } = this.state;
+    let {
+      // active state
+      text,
+      style,
+      className = 'btn btn-danger',
+    } = this.props;
+
+    const isDisabled = this.isDisabled();
+    const isConfirming = this.isConfirming();
+    const isActive = this.isActive();
+
+    if (isDisabled) {
+      text = disabled && disabled.text || 'Loading...';
+      className = disabled && disabled.className || 'btn btn-secondary';
+      style = disabled && disabled.style || {};
+    }
+    if (isConfirming) {
+      text = confirming && confirming.text || 'Confirm?';
+      className = confirming && confirming.className || 'btn btn-warning';
+      style = confirming && confirming.style || {};
+    }
+
     return (
       <button
-        className={classnames(
-          'confirm-button',
-          { 'confirm-button-confirming': clicked },
-          { [classNameConfirm]: clicked }
-        )}
-        style={(clicked ? styleConfirm : style)}
-        onClick={this.onConfirmClicked}
+        className={classnames('confirm-button', className)}
+        style={style}
+        onClick={this.onClickToggleState}
+        disabled={isDisabled}
+        {...buttonProps}
       >
         {children}
-        {clicked ? textConfirm : text}
+        {children ? ' ' : ''}
+        {text}
       </button>
     );
   }
 }
 ConfirmButton.propTypes = {
-  children: React.PropTypes.string.isRequired,
-  // user passed in function to be triggered after confirmed
+  // if true, we will disable the button after confirming & disabled
+  // if false || empty, we will loop around and allow click & confirm again
+  disableAfterConfirmed: React.PropTypes.bool,
+  // user passed in function - on confirmation
+  onConfirm: React.PropTypes.func,
+  // user passed in function - on disable, after onConfirm
+  onDisable: React.PropTypes.func,
+  // user passed in function - on click, before confirmation
   onClick: React.PropTypes.func,
-  style: React.PropTypes.object,
-  styleConfirm: React.PropTypes.object,
+  // displayed normally, before confirming, while active
+  text: React.PropTypes.oneOfType([
+    React.PropTypes.string,
+    React.PropTypes.node,
+  ]),
   className: React.PropTypes.string,
-  classNameConfirm: React.PropTypes.string,
-  text: React.PropTypes.string,
-  textConfirm: React.PropTypes.string,
+  style: React.PropTypes.object,
+  // displayed only while confirming
+  confirming: React.PropTypes.shape({
+    text: React.PropTypes.oneOfType([
+      React.PropTypes.string,
+      React.PropTypes.node,
+    ]),
+    className: React.PropTypes.string,
+    style: React.PropTypes.object,
+    // alias for onConfirm (convenience)
+    onClick: React.PropTypes.func,
+  }),
+  // displayed only after confirmed (clicked twice, disabled)
+  disabled: React.PropTypes.shape({
+    text: React.PropTypes.oneOfType([
+      React.PropTypes.string,
+      React.PropTypes.node,
+    ]),
+    className: React.PropTypes.string,
+    style: React.PropTypes.object,
+  }),
+  // children always displayed (optional)
+  children: React.PropTypes.string,
+  // custom props to pass into button
+  buttonProps: React.PropTypes.object,
 };
 
--- src/stories/index.js
+++ src/stories/index.js
@@ -4,18 +4,77 @@ import ConfirmButton from '../index';
 
 storiesOf('Confirm Button', module)
   .add('default view', () => (
+    <ConfirmButton>Normal Button</ConfirmButton>
+  ))
+  .add('using onConfirm & onClick', () => (
     <ConfirmButton
-      onClick={ action('button clicked') }
-      text="do stuff"
-      textConfirm="are you sure?"
+      onConfirm={ action('triggered onConfirm') }
+      onClick={ action('triggered onClick') }
+      text="Do stuff"
+      textConfirm="Are you sure?"
+    />
+  ))
+  .add('disabling with custom classNames for states', () => (
+    <ConfirmButton
+      disableAfterConfirmed
+      text="Button is Active"
+      className="btn btn-primary btn-lg"
+      confirming={{
+        text: 'Button is Confirming',
+        className: 'btn btn-info btn-sm',
+      }}
+      disabled={{
+        text: 'Button is Disabled',
+        className: 'btn btn-secondary text-muted',
+      }}
+    />
+  ))
+  .add('disable button after confirmed', () => (
+    <ConfirmButton
+      disableAfterConfirmed
+      text="Do stuff"
+      confirming={{
+        text: 'Do ya really wanna?',
+      }}
+      disabled={{
+        text: 'Doing stuff... assume it will remove this component',
+      }}
     />
   ))
   .add('with children - always displays', () => (
     <ConfirmButton
-      onClick={ action('button clicked') }
+      onConfirm={ action('confirmed') }
       text="do stuff"
       textConfirm="are you sure?"
     >
-      Here is child text --&nbsp;
+      Here is child text
     </ConfirmButton>
+  ))
+  .add('text as react/jsx nodes', () => (
+    <ConfirmButton
+      onConfirm={ action('button clicked') }
+      text={(
+        <span>
+          Go ahead,&nbsp;
+          <em>click this button</em>
+        </span>
+      )}
+      confirming={{
+        text: (
+          <span>
+            Are you&nbsp;
+            <em>absolutly</em>&nbsp;
+            <strong>sure?</strong>
+          </span>
+        ),
+      }}
+      disabled={{
+        text: (
+          <span>
+            Sad face
+            <em>DISABLED</em>
+          </span>
+        ),
+      }}
+    />
   ));
